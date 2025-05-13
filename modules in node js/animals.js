929ef4ac24c6174cd7d86f26c9fdb630b1e17df4
darvesh soni <darveshsoni1@gmail.com>
2025-05-13 12:58:17 +0530

    refactor: Remove unnecessary code related to file system and modules

--- modules in node js/animals.js
+++
@@ -1,9 +0,0 @@
-const elephantWeight = 1000;
-const lionWeight = 150;
-
-
-const sumOFWeights = () => {
-    return elephantWeight + lionWeight
-}
-
-module.exports = {sumOFWeights, elephantWeight, lionWeight};
--- modules in node js/index.js
+++
@@ -1,5 +0,0 @@
-const { elephantWeight, sumOFWeights, lionWeight } = require('./animals.js');
-console.log(elephantWeight);
-console.log(sumOFWeights());
-console.log(lionWeight); 
-console.log(lionWeight+sumOFWeights());
