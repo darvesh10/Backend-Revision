ea398435e14309abe1c29a2d7895a545b585610e
Darvesh Soni <darveshsoni1@gmail.com>
2025-07-17 19:10:27 +0530

    Update animals.js

--- animals.js
+++ animals.js
@@ -6,4 +6,4 @@ const sumOFWeights = () => {
 //in this filre we are importing the elephantWeight, sumOFWeights and lionWeight from animals.js file
 // to use them in index.js file.
 //this concept is called module.exports.
-module.exports = {sumOFWeights, elephantWeight, lionWeight};
+module.exports = {sumOFWeights, elephantWeight, lionWeight};// make sure about syntax
